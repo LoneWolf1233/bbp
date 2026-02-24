f45890f39bf8337c9c90d02a4e32a97a3d51523b
Sensei Whou <johnvit84@gmail.com>
2026-02-24 22:34:12 +0200

    deleted:    Batch/1.bat 	deleted:    Batch/1.rar 	deleted:    Batch/2.bat 	deleted:    Batch/3.bat 	deleted:    Batch/toolbox.bat 	modified:   config.env 	deleted:    fix_line_endings.sh 	deleted:    ghauri.egg-info/PKG-INFO 	deleted:    ghauri.egg-info/SOURCES.txt 	deleted:    ghauri.egg-info/dependency_links.txt 	deleted:    ghauri.egg-info/entry_points.txt 	deleted:    ghauri.egg-info/not-zip-safe 	deleted:    ghauri.egg-info/requires.txt 	deleted:    ghauri.egg-info/top_level.txt 	deleted:    olacabs.txt 	deleted:    "\316\235\316\255\316\277 \316\210\316\263\316\263\317\201\316\261\317\206\316\277 \316\272\316\265\316\271\316\274\316\255\316\275\316\277\317\205.txt"

--- Batch/1.bat
+++
@@ -1,7 +0,0 @@
-@ECHO off
-chcp 65001
-msg %username% "Καλημέρα πατέρα."
-msg %username% "Αν βλέπεις αυτό το μήνυμα τότε το πείραμα πέτυχε."
-msg %username% "Μαθαίνω την γλώσσα των Windows."
-msg %username% "Αυτά..."
-msg %username% "Ραντεβού στην Άρσεναλ και με την νίκη!"
--- Batch/2.bat
+++
@@ -1,9 +0,0 @@
-@ECHO off
-chcp 65001
-msg %username% "Γεια σου Άρη."
-msg %username% "Πώς πάει;."
-msg %username% "26 μέρες για το σχολείο."
-msg %username% "Ναιιιιι."
-msg %username% "Εννοώ όχιιιιιιι!!"
-msg %username% "Γράψε διήγημα."
-msg %username% "Bye!"
